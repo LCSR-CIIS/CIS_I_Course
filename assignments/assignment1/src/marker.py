46b8769267893cac44fe023dc7fa5fbbaf0b6797
Aiza Maksutova <aiza.maksutova@milwaukeetool.com>
2026-07-26 22:33:03 +0000

    Added markers class

--- assignments/assignment1/src/marker.py
+++ assignments/assignment1/src/marker.py
@@ -1,1 +1,31 @@
 # they can update the nominal values relative to the marker body - should be able to do that on the api side. 
+from typing import List
+
+from uncertainty_networks.nominal_types import vct3, Frame
+from .utils import sample_normal
+from tracker import Tracker
+
+
+class Markers:
+
+    def __init__(self, name):
+        self.name = name
+        self.nominal_marker_positions = [] # vct3 or uvct3 type; error-free marker positions
+        self.observed_marker_positions = [] # vct3 type
+        self.C = None
+
+    def set_cov(self, C):
+        self.C = C
+        return self
+
+    def create_marker_body(self, nominal_marker_positions: List[vct3]):
+        self.nominal_marker_positions = nominal_marker_positions
+        # here sampling can also depend on the params of uvct3?
+        self.observed_marker_positions = sample_normal(nominal_marker_positions, cov=self.C)
+        return self
+
+    def add_marker_positions(self, nominal_marker_positions: List[vct3]):
+        for pos in nominal_marker_positions:
+            self.nominal_marker_positions.append(pos)
+            self.observed_marker_positions.append(sample_normal([pos], cov=self.C)[0])
+        return self
--- assignments/assignment1/src/tracker.py
+++ assignments/assignment1/src/tracker.py
@@ -2,6 +2,7 @@ from typing import List
 
 from .utils import sample_normal
 from uncertainty_networks.nominal_types import vct3, Frame
+from marker import Markers
 
 
 class Tracker:
@@ -10,9 +11,8 @@ class Tracker:
         self.name = name
         self.F = None  # Frame position of the tracking camera in the world coordinate system
         self.C = None  # Covariance matrix representing the uncertainty of the tracker's pose
-        # keep track of the all marker positions that the tracker sees
         self.marker_positions = []
-        # marker should be able to add new points to the list of seen marker positions
+
     def __repr__(self):
         return f"Tracker(name={self.name!r}, F={self.F!r}, C={self.C!r})"
 
@@ -31,18 +31,18 @@ class Tracker:
         self.C = cov
         return self
 
-    def read_markers(self, marker_pos: List[vct3]):
+    def read_markers(self, marker: "Markers"): # CHANGE: here it should read a Marker object instead of raw positions
         # read the marker positions and update the tracker's state accordingly
         '''
-        input: marker_pos - list of marker positions in the world coordinate system
+        input: marker - a Marker object containing nominal and observed marker positions
         output: samples - list of sampled marker positions
         '''
-        n = len(marker_pos)
+        n = len(marker.observed_marker_positions)
         samples = []
         for i in range(n):
-            marker_pos_tracker = self.F.inv() * marker_pos[i]
+            marker_pos_tracker = self.F.inv() * marker.observed_marker_positions[i]
             marker_pos_tracker_sampled = sample_normal(
-                marker_pos_tracker.vec.squeeze(1), self.C)
+                marker_pos_tracker.vec.squeeze(1), cov=self.C)
             samples.append(marker_pos_tracker_sampled)
         return samples
 
